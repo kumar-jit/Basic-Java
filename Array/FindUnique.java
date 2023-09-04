4c15df0f349cbdc8b5137cbc95ea55bda91dfc65
Kumarjit <kumarjeetmaity99@gmail.com>
2023-09-04 23:08:53 +0530

    Find duplicate

--- Array/FindUnique.java
+++ Array/FindUnique.java
@@ -67,8 +67,10 @@ public class FindUnique {
         int no = 0;
         for(int i=0;i< arr.length;i++){
             for(int j=0;j< arr.length;j++){
-                if(i!=j && arr[i]==arr[j])
+                if (i != j && arr[i] == arr[j]) {
                     status = true;
+                    break;
+                }
             }
             if(status){
                 status = false;
