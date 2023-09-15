abcd161a64baf303101d5d410ab94e51d3de31d0
Kumarjit <kumarjeetmaity99@gmail.com>
2023-09-15 21:04:24 +0530

    Maximise Sum

---
+++ Array/MaximiseTheSum.java
@@ -0,0 +1,53 @@
+package Array;
+
+import java.util.Scanner;
+
+public class MaximiseTheSum {
+    public static Scanner sc = new Scanner(System.in);
+    public static int[] takeInput(){
+        int length = sc.nextInt();
+        int arr[] = new int[length];
+        for (int i = 0;i<length;i++){
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maximumSumPath(takeInput(),takeInput()));
+    }
+
+    public static long maximumSumPath(int[] input1, int[] input2) {
+
+        long totalSum = 0,sum1=0,sum2=0;
+        int p1=0,p2=0;
+
+        while (p1<input1.length && p2<input2.length){
+            if(input1[p1]<input2[p2]){
+                sum1 += input1[p1++];
+            }
+            else if (input1[p1]>input2[p2]){
+                sum2 += input2[p2++];
+            }
+            else {
+                sum1 += input1[p1++];
+                sum2 += input2[p2++];
+
+                totalSum += Math.max(sum1, sum2);
+                sum1 = 0;
+                sum2 = 0;
+            }
+        }
+
+        while (p1<input1.length){
+            sum1 += input1[p1++];
+        }
+        while (p2<input2.length){
+            sum2 += input2[p2++];
+        }
+
+        totalSum += Math.max(sum1,sum2);
+        return totalSum;
+    }
+
+}
--- Array/Sort_0_1.java
+++ Array/Sort_0_1.java
@@ -21,28 +21,65 @@ public class Sort_0_1 {
         }
     }
 
+//    public static void sort01(int[] arr){
+//        int count0=0,count1=0;
+//        for(int i=0;i<arr.length;i++){
+//            if(arr[i]==0){
+//                count0++;
+//                continue;
+//            }
+//            if(arr[i]==1){
+//                count1++;
+//                continue;
+//            }
+//        }
+//        for(int i=0;i<arr.length;i++){
+//            if(count0>0){
+//                arr[i]=0;
+//                --count0;
+//                continue;
+//            }
+//            if (count0==0 && count1>0){
+//                arr[i]=1;
+//                --count1;
+//            }
+//        }
+//    }
+
     public static void sort01(int[] arr){
-        int count0=0,count1=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==0){
-                count0++;
-                continue;
+        int left = 0,right= arr.length-1;
+
+        // --- best approach ------------
+        while (left<=right){
+            if(left==0 && right==0){
+                left++;
             }
-            if(arr[i]==1){
-                count1++;
-                continue;
+            else if (left==0 && right==1){
+                left++;right--;
             }
-        }
-        for(int i=0;i<arr.length;i++){
-            if(count0>0){
-                arr[i]=0;
-                --count0;
-                continue;
-            }
-            if (count0==0 && count1>0){
-                arr[i]=1;
-                --count1;
+            else if(left==1 && right ==1){
+                right--;
+            }else {
+                swap(arr,left,right);
+                left++;right--;
             }
         }
+
+// ---------- take more time to execute -------------
+//        while (left<=right){
+//            if(arr[left]==0){
+//                left++;
+//            }
+//            else {
+//                swap(arr,left,right);
+//                right--;
+//            }
+//        }
+    }
+
+    public static void swap(int[] arr,int position1,int position2){
+        int backup = arr[position1];
+        arr[position1] = arr[position2];
+        arr[position2] = backup;
     }
 }
